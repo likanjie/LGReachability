54f6a05ab0bf7881baec7436a0694e5083f129c4
likanjie <likanjie@likanjiedeMacBook-Pro.local>
2016-03-01 17:55:09 +0800

    AFN封装实时更新网络状态
    实时监听网络状态，在无线网络、蜂窝数据、无网络下可以自动切换不同页面

---
+++ LGReachability/LGReachability/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LGReachability
+//
+//  Created by 李堪阶 on 16/3/1.
+//  Copyright © 2016年 DM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
